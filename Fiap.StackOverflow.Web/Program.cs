6be3e0e83a13e8863397c9bd11b96d9cf9c7d8bf
jonassaito <jonassaitobr@gmail.opm>
2018-11-05 00:59:32 -0200

    Criando projeto web do zero

---
+++ Fiap.StackOverflow.Web/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fiap.StackOverflow.Web
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
