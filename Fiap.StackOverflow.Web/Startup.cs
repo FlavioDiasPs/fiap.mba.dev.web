8396c1567247f38a8fdeb743f1b4555d8df9b61c
jonassaito <jonassaitobr@gmail.com>
2018-11-12 03:53:14 -0200

    Ajustes de connectionString e layout

--- Fiap.StackOverflow.Web/Startup.cs
+++ Fiap.StackOverflow.Web/Startup.cs
@@ -36,7 +36,7 @@ namespace Fiap.StackOverflow.Web
         {
             serviceCollection.AddAutoMapper();            
 
-            serviceCollection.AddDbContext<StackOverflowContext>(options => options.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Jonas\source\repos\Fiap.StackOverflow.Web\Fiap.StackOverflow.Data\App_Data\dbStackOverflow.mdf;Integrated Security=True;Connect Timeout=30"));
+            serviceCollection.AddDbContext<StackOverflowContext>(options => options.UseSqlServer(ConnectionString));
 
             //serviceCollection.AddDefaultIdentity<IdentityUser>()
             //    .AddEntityFrameworkStores<StackOverflowContext>();
