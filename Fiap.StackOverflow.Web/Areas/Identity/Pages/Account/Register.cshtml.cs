c53bbe9aafb29cd0a86c7a155691af8f2bb53513
jonassaito <jonassaitobr@gmail.com>
2018-11-14 00:02:57 -0200

    layout

--- Fiap.StackOverflow.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Fiap.StackOverflow.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -90,11 +90,12 @@ namespace Fiap.StackOverflow.Web.Areas.Identity.Pages.Account
                     ,LastName = Input.LastName
                 };
                 var result = await _userManager.CreateAsync(user, Input.Password);
-                if (result.Succeeded)
+                if (result.Succeeded) 
                 {
                     _logger.LogInformation("User created a new account with password.");
+                    var name = string.IsNullOrWhiteSpace(user.FirstName) ? user.UserName : $"{user.FirstName} {user.LastName}";
 
-                    var author = new Author($"{user.FirstName} {user.LastName}", user.Id);
+                    var author = new Author(name, user.Id);
 
                     _authorService.Add(author);
 
